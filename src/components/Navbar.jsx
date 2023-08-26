fb3b984d0887a39455be7fbee062b3ae01b892af
Anya <anyasanders1@gmail.com>
2023-08-26 10:01:03 -0500

    clean up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,18 +17,15 @@ return (
         <nav className="navbarNav">
             <ul className="navbarListItems">
                 <li className="navbarListItemHome" onClick={() => navigate('/')}>
-                    {/* <HomeIcon fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px' /> */}
                     <p className={pathMatchRoute('/') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Home</p>
                 </li>
                 <li className="navbarListItemExplore" onClick={() => navigate('/explore')}>
-                    {/* <RecipeIcon fill={pathMatchRoute('/explore') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px' /> */}
                     <p className={pathMatchRoute('/explore') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Explore</p>
                 </li>
                 <li  className="navbarListItemLogo" onClick={() => navigate('/')}>
                 <img src={logo} alt="logo" className="navBarLogo"/>
                 </li>
                 <li className="navbarListItemProfile" onClick={() => navigate('/profile')}>
-                    {/* <PersonOutlineIcon fill={pathMatchRoute('/profile') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px' /> */}
                     <p className={pathMatchRoute('/profile') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Profile</p>
                 </li>
             </ul>
