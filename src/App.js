f7cc900d6a46e835c465013676532226b78f0aa8
Anya <anyasanders1@gmail.com>
2023-09-04 13:14:13 -0500

    formatting

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ import EditListing from "./pages/EditListing";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css'
 
-
-
 function App() {
   return (
     <>
